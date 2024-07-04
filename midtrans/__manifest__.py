7754649bdb7d6dc40037bcdc4020b2ff8f56e699
stevenchan7 <0stevenchan0@gmail.com>
2024-07-04 15:48:17 +0800

    initialize project

---
+++ midtrans/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Midtrans Payment Module',
+    'version': '1.0',
+    'summary': 'Module support midtrans payment gateway',
+    'author': ['Steven Chan', 'Gemuh Raharja'],
+    'category': 'Uncategorized',
+    'depends': ['payment'],
+    'data': [
+        # XML files for views, actions, etc.
+        # 'views/payment_midtrans_template.xml',
+        # 'views/midtrans_view.xml',
+        # 'data/payment_acquirer_data.xml',
+        # 'views/assets.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
